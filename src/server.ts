8f624c4032157e5b68b2b60769ddf140b4c28912
Antonette Caldwell <pullmana8@gmail.com>
2022-01-15 14:01:02 -0600

    Moved some files around

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,29 @@
 import express, { Application, Request, Response } from 'express'
+import { filterImageFromURL } from './util/util'
 
 (async () => {
     const app: Application = express()
     const port = process.env.PORT || 8082
 
-    app.use(express.urlencoded({extended: true}))
+    app.use(express.urlencoded({ extended: true }))
     app.use(express.json())
 
     app.get('/', (req: Request, res: Response) => {
         res.send(`Try GET /filteredimage?image={{}}`)
     })
 
-    app.get('/filteredimage', async(req: Request, res: Response) => {
+    app.get('/filteredimage', async (req: Request, res: Response) => {
         let { image_url } = req.query as any
 
-        if(!image_url) {
+        if (!image_url) {
             return res.status(400)
                 .send(`Invalid URL or no image URL provided`)
         }
+
+        filterImageFromURL(image_url).then(filteredpath => {
+            res.status(200)
+                .sendFile(filteredpath)
+        })
     })
 
     app.listen(port, () => {
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,19 +1,39 @@
+import fs from "fs"
 import sharp from "sharp"
 
 export async function filterImageFromURL(inputURL: string): Promise<string> {
     return new Promise(async resolve => {
+        let temp_dir = '/tmp'
+
+        if (!fs.existsSync(temp_dir)) {
+            fs.mkdirSync(temp_dir, {
+                recursive: true
+            })
+        }
+
         const photo = sharp(inputURL)
         const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
 
-        await photo.resize(256, 256)
-            .jpeg({
-                quality: 60
-            })
-            .grayscale()
-            .toFile(__dirname + outpath, (img) => {
-                resolve(__dirname + outpath)
-            })
+        console.log(outpath)
 
-        console.log(photo)
+        photo
+            .metadata()
+            .then((metadata) => {
+                return photo
+                    .resize(Math.round(metadata.width), 256)
+                    .jpeg({
+                        quality: 60
+                    })
+                    .grayscale()
+                    .toFile(`${outpath}`, (img) => {
+                        resolve(__dirname + outpath)
+                    })
+            })
     })
+}
+
+export async function deleteLocalFiles(files: Array<string>) {
+    for (let file of files) {
+        fs.unlinkSync(file)
+    }
 }
