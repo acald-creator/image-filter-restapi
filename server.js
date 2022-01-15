733edf60049e32a171d56da2c7f142bcf80c38ce
Antonette Caldwell <pullmana8@gmail.com>
2022-01-15 12:03:48 -0600

    Starting over

--- server.js
+++ server.js
@@ -1,48 +1,11 @@
-/* import express from 'express'
-import chokidar from 'chokidar'
-*/
-
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-
-const chokidar = require('chokidar')
-const watcher = chokidar.watch('./app')
-
-const resize = require('./app/resize')
-
-app.use(function (req, res, next) {
-    require('./app/index')(req, res, next)
-})
-
-watcher.on('ready', function () {
-    watcher.on('all', function () {
-        console.log('Clearing /dist/ module cache from server')
-        Object.keys(require.cache).forEach(function (id) {
-            if (/[\/\\]app[\/\\]/.test(id)) delete require.cache[id]
-        })
-    })
-})
+const port = 3000
 
 app.get('/', (req, res) => {
-    const widthString = req.query.width
-    const heightString = req.query.height
-    const format = req.query.format
-
-    let width, height
-    if (widthString) {
-        width = parseInt(widthString)
-    }
-
-    if(heightString) {
-        height = parseInt(heightString)
-    }
-
-    res.type(`image/${format || 'png, jpg'}`)
-
-    resize('unsplash.png', format, width, height).pipe(res)
+    res.send('Hello World!')
 })
 
-app.listen(port, function () {
-    console.log('Server is running on PORT', port)
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
 })
